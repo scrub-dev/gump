de51814e9b7022283e429c58ad0c2291390c8a14
scrub <scott-donaldson@outlook.com>
2024-10-01 17:00:01 +0100

    Attempt #1 of bypassing wsl permissions with stdin inputs...

--- utils/service/enable.py
+++ utils/service/enable.py
@@ -1,11 +1,15 @@
 import win32serviceutil
 import utils
+import utils.service
+import utils.wsl
+import utils.wsl.instance
+import utils.wsl.online
 
 def execute(service) -> bool :
     if service['env'] == 'win':
         win32serviceutil.StartService(service['name'])
     elif service['env'] == 'wsl':
-        if utils.wsl.online.check(): utils.wsl.instance.run(f"sudo systemctl start {service['name']}")
+        if utils.wsl.online.check(): utils.wsl.instance.runWithInput(f"sudo systemctl start {service['name']}", "")
         else: utils.printer.console(f"WSL Instance not running... ignoring {service['name']}")
         return
     return True
--- utils/wsl/instance.py
+++ utils/wsl/instance.py
@@ -1,5 +1,8 @@
 import subprocess
 import re
+
+import pexpect
+import pexpect.popen_spawn
 import utils
 import utils.printer
 import utils.wsl
@@ -9,7 +12,15 @@ def run(command):
     if utils.wsl.online.check():
         return subprocess.check_output(f"wsl exec /bin/bash -c \"{command}\"", text=True, shell=True)
     else: utils.printer.console(f"WSL Instance offline, not running wsl command",2)
+def runWithInput(command, input):
+    if utils.wsl.online.check():
+        return subprocess.check_output(f"wsl exec /bin/bash -c \"{command}\"", text=True, shell=True)
 
+        # x = pexpect.popen_spawn.PopenSpawn(f"wsl exec /bin/bash -c \"{command}\"", input="Core!")
+        # x.expect("password")
+        # x.sendline("Core!")
+        return
+    else: utils.printer.console(f"WSL Instance offline, not running wsl command",2)
 def getServiceStatus(serviceName):
     x = run(f"service --status-all | grep -i {serviceName}").strip()
     return re.search("\[(.*?)\]", x).group(1).strip() == "+"
