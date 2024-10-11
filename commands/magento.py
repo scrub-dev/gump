14b3419aaadafbd25f8354fb75b3fed4a449148b
scrub <scott-donaldson@outlook.com>
2024-10-11 11:47:32 +0100

    Commands now execute individually at GUMP level and not subproces level Including a pretty printer update to make it look nicer

--- commands/magento.py
+++ commands/magento.py
@@ -3,8 +3,10 @@ import itertools
 import json
 import utils
 import utils.getFile
+import subprocess
+
+import utils.printer
 #bin/magento s:up && bin/magento setup:di:compile && bin/magento setup:static-content:deploy -f && bin/magento c:f 
-REQUIRE_ADMIN = True
 
 def main(params: list) -> None:
     parser = argparse.ArgumentParser()
@@ -18,7 +20,7 @@ def main(params: list) -> None:
     # removes invalid commands, checks for aliases first, creates a flat array of all command shorthands,
     # alias refers to shorthand, not full commands
     # making everything a single nested list comprehension if possible because... its still more readable than magento code...
-    commands = " && ".join([f"{conf['environments'][conf['magento_env']]} {conf['magento_site_root']}{args.site}/{conf['magento_site_bin']} {x}" 
+    commands = ([f"{conf['environments'][conf['magento_env']]} {conf['magento_site_root']}{args.site}/{conf['magento_site_bin']} {x}" 
         for x in list(set([conf["commands"][x] for x in list(
             itertools.chain.from_iterable(
                 e for e in [conf["aliases"][command].split(",") 
@@ -26,10 +28,20 @@ def main(params: list) -> None:
                 else [command] 
                 for command in args.commands.split(",")]
             )) if x in conf["commands"]]))])
-    
+    # print(commands)#
 
-    print(commands)
-    # execute command
+    utils.printer.console(f"Site: {args.site}")
+    utils.printer.console(f"Site Environment: {conf['magento_env']}")
+    utils.printer.console(f"Site Location: {conf['magento_site_root']}{args.site}/")
+    utils.printer.console("Command" + ('s' if len(commands) > 1 else ''))
+    [utils.printer.console(c,2) for c in commands]
+    print()    
+    # subprocess.call(commands)
+
+    [subprocess.call(command) for command in commands]
+
+
+    print("Done!")
 
     return
 
