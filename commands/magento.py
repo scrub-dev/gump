49e0436aa46f057918e227f60ced8dc52653aa14
scrub <scott-donaldson@outlook.com>
2024-10-11 11:09:20 +0100

    Magento Automatic Script Generator Command Decided to make one massive nested list comprehension because why not, its friday... Im going to look at this on monday and cry...

--- commands/magento.py
+++ commands/magento.py
@@ -1,1 +1,38 @@
-#bin/magento s:up && bin/magento setup:di:compile && bin/magento setup:static-content:deploy -f && bin/magento c:f
+import argparse
+import itertools
+import json
+import utils
+import utils.getFile
+#bin/magento s:up && bin/magento setup:di:compile && bin/magento setup:static-content:deploy -f && bin/magento c:f 
+REQUIRE_ADMIN = True
+
+def main(params: list) -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("site", type=str)
+    parser.add_argument("commands", type=str)
+    args = parser.parse_args(params)
+
+    with open(utils.getFile.config("conf.json", "magento")) as conf:
+        conf = json.load(conf)
+
+    # removes invalid commands, checks for aliases first, creates a flat array of all command shorthands,
+    # alias refers to shorthand, not full commands
+    # making everything a single nested list comprehension if possible because... its still more readable than magento code...
+    commands = " && ".join([f"{conf['environments'][conf['magento_env']]} {conf['magento_site_root']}{args.site}/{conf['magento_site_bin']} {x}" 
+        for x in list(set([conf["commands"][x] for x in list(
+            itertools.chain.from_iterable(
+                e for e in [conf["aliases"][command].split(",") 
+                if command in conf["aliases"] 
+                else [command] 
+                for command in args.commands.split(",")]
+            )) if x in conf["commands"]]))])
+    
+
+    print(commands)
+    # execute command
+
+    return
+
+def help():
+
+    return 
--- configs/bump_version.py
+++ configs/bump_version.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     parser.add_argument('-o','--overwrite', type=str, required=False)
     parser.add_argument('-m','--minor', action='store_true')
     parser.add_argument('-mm','--major', action='store_true')
+    parser.add_argument('-p', '--patch', action='store_true')
 
     args = parser.parse_args()
     print(args)
