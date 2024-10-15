5a04db522453dc11fcac4c4c6d10bc8e589df619
scrub <scott-donaldson@outlook.com>
2024-10-15 11:24:25 +0100

    Created Workspace command file with boilerplate structure

---
+++ commands/workspace.py
@@ -0,0 +1,9 @@
+# gump password length [optional charsets / default A-Za-z0-9]
+import argparse
+import logging
+logger: logging.Logger
+
+def main(parameters) -> None:
+    return
+def help() -> None:
+    return
