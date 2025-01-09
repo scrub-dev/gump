f294b3b74e2b88cc99497f8cbf41e13a24fa2444
scrub <scott-donaldson@outlook.com>
2025-01-09 09:26:38 +0000

    Added open command with presets

--- commands/open.py
+++ commands/open.py
@@ -1,37 +1,35 @@
+import json
 import logging
 import argparse
+import subprocess
+import utils
 
 logger: logging.Logger
 
 def main (parameters: list) -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("main", help="Provide a preset, url or site name")
-    parser.add_argument("-t", "--type", help="Type of site (core/magento/etc) Not needed if providing a preset or url", required=False)
     parser.add_argument("-b", "--browser", help="Open a specific browser or enter 'all' to open all browsers", required=False)
-    parser.add_argument("-n", "--name", help="Require a specific basename for site name, use", required=False)
-
     parser.parse_args(parameters)
+    args = parser.parse_args(parameters)
 
-    # create a toOpen array
+    with open(utils.getFile.config("conf.json", "open")) as conf:
+        conf = json.load(conf)
+    
+    browser_exe = \
+            next((x for x in conf["browsers"] if x['name'] == args.browser), None) \
+            if args.browser and args.browser in [x['name'] for x in conf['browsers']] \
+            else next(x for x in conf["browsers"] if x['name'] == conf['default_browser'])
+    
+    with open(utils.getFile.config("presets.json", "open")) as presets:
+        presets = json.load(presets)
+    websites_to_open = ([f for f in presets['presets'] if f['name'] == args.main])[0]['urls'] if args.main in [x['name'] for x in presets['presets']] else [args.main]
 
-    # Look for preset
-        # do the preset stuff
-        # similar to check if sitename ngl
+    subprocess.run(formatOpenCommand(browser=browser_exe['exe_location'], websites=websites_to_open), shell=True)
 
-    # Check if url
-        # add url to toOpen array
+def formatOpenCommand(browser, websites):
+    return f"start {browser} {' '.join(websites)}"
+    
 
-    # Check if sitename
-        # check if type set, if not use default
-        # check if name set, if not use default
-        # get type structure and populate with values
-        # get type endpoints
-        # add type_structure + type_endpoints, per endpoint to toOpen array
-
-    # Check if browser set
-        #if all, run open with all browsers
-        #if set, open with set browser
-        #if not set, open with default browser
-
-def help() -> None:
-    print("Open Command!")
+def help () -> None:
+    print("Opens websites")
